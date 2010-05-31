9efee0d732fb0e685c66a1d3891ccecf9418df64
Mons Anderson <mons@cpan.org>
2010-06-01 02:21:16 +0400

    fix bsd version

--- tmp/compile.sh
+++ tmp/compile.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-#gcc -lkvm -o test test.c
-gcc -fno-common -ffast-math \
-  -W -Wall -Wshadow -Wcast-align -Wredundant-decls \
-  -Wbad-function-cast -Wcast-qual -Wwrite-strings -Waggregate-return \
-  -Wstrict-prototypes -Wmissing-prototypes -o test test.c
+CFLAGS="-O2 -g -ggdb -fno-common -ffast-math -W -Wall -Wshadow -Wcast-align -Wredundant-decls -Wbad-function-cast -Wcast-qual -Wwrite-strings -Waggregate-return -Wstrict-prototypes -Wmissing-prototypes"
+
+gcc -lkvm -o test test.c
+#gcc $CFLAGS -o test test.c
